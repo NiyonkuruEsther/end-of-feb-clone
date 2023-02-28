ee6ff9cff164fe8b24f61eee97729515028bd9ad
estherniyo <estherniyonkuru123@gmail.com>
2023-02-28 18:17:34 +0200

    the first code refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,20 @@ import { CiHeart } from "react-icons/ci";
 import { IoGlobeSharp } from "react-icons/io5";
 import { RxCross1 } from "react-icons/rx";
 import { FaResearchgate } from "react-icons/fa";
+import { AiOutlineHeart, AiOutlineGlobal, AiOutlinePlus } from "react-icons/ai";
+import { MdClose, MdKeyboardArrowUp } from "react-icons/md";
+import { FiSearch } from "react-icons/fi";
+
+import { FaGraduationCap } from "react-icons/fa";
+import { TbFilePencil } from "react-icons/tb";
+import { BsFillMenuButtonFill } from "react-icons/bs";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(0);
   const [sticky, setSticky] = useState(scrolled);
   const [slide, setSlide] = useState(1);
   const [tabValue, setTabValue] = useState(0);
+  const [menuValue, setMenuValue] = useState(0);
 
   useEffect(() => {
     const changescrolled = () => {
@@ -39,6 +47,10 @@ const Navbar = () => {
     document.body.style.overflow = tabValue ? "hidden" : "auto";
     return () => (document.body.style.overflow = "scroll");
   }, [tabValue]);
+  useEffect(() => {
+    document.body.style.overflow = tabValue ? "hidden" : "auto";
+    return () => (document.body.style.overflow = "scroll");
+  }, [menuValue]);
   return (
     <>
       {" "}
@@ -99,15 +111,28 @@ const Navbar = () => {
             </li>
             <Link
               href={""}
-              onClick={() => setTabValue(1)}
+              onClick={() => (setTabValue(1), setMenuValue(0))}
               className="flex items-center gap-2 hover:bg-lightGray p-1 "
             >
               <BiSearch />
 
               <span>Search</span>
             </Link>
-            <li className="flex items-center gap-2 hover:bg-lightGray p-1">
+            <li
+              onClick={() => (setMenuValue(1), setTabValue(0))}
+              className={`flex items-center gap-2 hover:bg-lightGray p-1 cursor-pointer ${
+                menuValue === 1 ? " hidden" : ""
+              }`}
+            >
               <BiMenu /> <span>Menu</span>
+            </li>
+            <li
+              className={`${
+                menuValue === 0 ? " hidden" : ""
+              } flex items-center gap-2 cursor-pointer hover:bg-lightGray`}
+              onClick={() => setMenuValue(0)}
+            >
+              <RxCross1 /> <span>Close menu</span>
             </li>
           </ul>
           <Link
@@ -158,8 +183,8 @@ const Navbar = () => {
       <div
         className={
           tabValue
-            ? "w-screen text-white absolute mx-auto h-screen transition-all ease-in-out duration-300 opacity-100 inset-0 z-[10000000] overflow-hidden gradient"
-            : "transition-all duration-0 absolute opacity-0 h-0 bottom-0 overflow-hidden mx-auto"
+            ? "w-screen text-white absolute mx-auto h-screen transition-all ease-in-out duration-300 opacity-100 inset-x-0 z-[10000000] overflow-hidden gradient"
+            : "absolute opacity-0 h-0 bottom-0 overflow-hidden mx-auto"
         }
       >
         <div className="max-w-3xl items-center pt-12 mx-auto flex w-full justify-between">
@@ -188,6 +213,111 @@ const Navbar = () => {
           <BiSearch size={25} />
         </div>
       </div>
+      {/* menu content */}
+      {menuValue ? (
+        <div
+          className={` auto fixed inset-x-0 right-0 z-[100] grid w-screen h-screen overflow-hidden bg-white bg-opacity-70`}
+        >
+          <div className=" w-[1100px] bg-mediumGray text-white right-0 fixed">
+            <div className="relative h-screen overflow-y-auto">
+              <div className="px-10 space-y-6  pt-36 ">
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center gap-12">
+                    {[
+                      {
+                        icon: <BsFillMenuButtonFill className="text-2xl" />,
+                        title: "Services",
+                      },
+                      {
+                        icon: <BsFillMenuButtonFill className="text-2xl" />,
+                        title: "Site index",
+                      },
+                      {
+                        icon: <FaGraduationCap className="text-2xl" />,
+                        title: "Student guide",
+                      },
+                      {
+                        icon: <TbFilePencil className="text-2xl" />,
+                        title: "Apply to Aalto",
+                      },
+                    ].map((e, i) => (
+                      <Link key={i} href={e?.title}>
+                        <div key={i} className="flex flex-col items-center ">
+                          {e?.icon}
+                          <p className="text-base leading-[1.9rem] text-center">
+                            {e?.title}
+                          </p>
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                  <p>
+                    Aalto community members please log in to see internal
+                    content
+                  </p>
+                </div>
+
+                <div className="flex flex-col justify-between cursor-pointer pb-12">
+                  {[
+                    "Admissions and applying",
+                    "News and events",
+                    "Research and art",
+                    "For students",
+                    "Schools and departments",
+                    "Tools",
+                    "About us",
+                    "Collaboration",
+                    ,
+                  ].map((item, i) => {
+                    return (
+                      <details
+                        key={i}
+                        // href={e}
+                        className="block border-y group "
+                      >
+                        <summary className="flex  hover:bg-lightGray items-center justify-between p-6 py-4 hover:bg-brandGray-light h-fit ">
+                          <p className="text-2xl font-bold leading-[30px] whitespace-nowrap ">
+                            {" "}
+                            {item}
+                          </p>
+
+                          <AiOutlinePlus className="text-2xl" />
+                        </summary>
+                        <div className="p-6 2xl:mr-40 grid grid-cols-3 gap-y-3">
+                          {[
+                            "Apply to Aalto",
+                            "Find a study programme",
+                            "How to apply",
+                            "Scholarships and tuition fees",
+                            "Bachelor's admissions",
+                            "Master's admissions",
+                            "Doctoral admissions",
+                            "Campus life",
+                            "Events for applicants",
+                            "Chat with students",
+                            "Subscribe to newsletter",
+                            "Lifewide Learning",
+                            "Contact admission services",
+                            "Exchange students",
+                          ].map((item, i) => (
+                            <Link
+                              key={i}
+                              href={item}
+                              className="p-1 underline hover:bg-lightGray w-fit "
+                            >
+                              <p className="text-sm">{item}</p>
+                            </Link>
+                          ))}
+                        </div>
+                      </details>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };
---
+++ src/data/menuData.jsx
@@ -0,0 +1,270 @@
+export const menuData = [
+  {
+    title: "Admissions and applying",
+    data: [
+      { title: "Apply to Aalto", link: "apply" },
+      { title: "Find a study programme", link: "find" },
+      { title: "How to apply", link: "how" },
+      {
+        title: "Scholarships and tuition fees",
+        link: "scholarships",
+      },
+      {
+        title: "Bachelors admissions",
+        link: "bachelors",
+      },
+      { title: "Masters admissions", link: "masters" },
+      {
+        title: "Doctoral admissions",
+        link: "doctoral",
+      },
+      { title: "Campus life", link: "campus" },
+      {
+        title: "Events for applicants",
+        link: "events",
+      },
+      { title: "Chat with students", link: "chat" },
+      {
+        title: "Subscribe to newsletter",
+        link: "subscribe",
+      },
+      { title: "Lifewide Learning", link: "lifewide" },
+      {
+        title: "Contact admission services",
+        link: "contact",
+      },
+      { title: "Exchange students", link: "exchange" },
+    ],
+  },
+  {
+    title: "News and events",
+    data: [
+      { title: "Highlights", link: "highlights" },
+      { title: "News", link: "news" },
+      { title: "Events", link: "events" },
+      {
+        title: "Aalto University Magazine",
+        link: "aalto",
+      },
+      { title: "OurBlogs", link: "ourblogs" },
+      { title: "Podcasts", link: "podcasts" },
+      {
+        title: "Information on coronavirus",
+        link: "information",
+      },
+      { title: "For the media", link: "for" },
+    ],
+  },
+  {
+    title: "Research and art",
+    data: [
+      { title: "Research and art", link: "research" },
+      {
+        title: "Teaching and learning for teachers",
+        link: "teaching",
+      },
+      {
+        title: "Research portal researchaaltofi",
+        link: "research",
+      },
+      { title: "Doctoral education", link: "doctoral" },
+      {
+        title: "Doctoral programmes",
+        link: "doctoral",
+      },
+      { title: "Tenure track", link: "tenure" },
+      {
+        title: "Research and learning facilities",
+        link: "research",
+      },
+      {
+        title: "Aalto Networking Platform",
+        link: "aalto",
+      },
+    ],
+  },
+  {
+    title: "For students",
+    data: [
+      { title: "The Student Guide", link: "the" },
+      { title: "Sisu", link: "sisu" },
+      { title: "MyCourses", link: "mycourses" },
+      { title: "MyStudies", link: "mystudies" },
+      { title: "Doctoral education", link: "doctoral" },
+      { title: "Browse all courses", link: "browse" },
+    ],
+  },
+  {
+    title: "Schools and departments",
+    data: [
+      {
+        title: "School of Arts Design and Architecture",
+        link: "school",
+      },
+      { title: "School of Business", link: "school" },
+      {
+        title: "School of Chemical Engineering",
+        link: "school",
+      },
+      {
+        title: "School of Electrical Engineering",
+        link: "school",
+      },
+      {
+        title: "School of Engineering",
+        link: "school",
+      },
+      { title: "School of Science", link: "school" },
+      { title: "Departments", link: "departments" },
+      {
+        title: "Accounting  Business Law",
+        link: "accounting",
+      },
+      { title: "Applied Physics", link: "applied" },
+      { title: "Architecture", link: "architecture" },
+      { title: "Art and Media", link: "art" },
+      {
+        title: "Bioproducts and Biosystems",
+        link: "bioproducts",
+      },
+      { title: "Built Environment", link: "built" },
+      {
+        title: "Chemical and Metallurgical Engineering",
+        link: "chemical",
+      },
+      {
+        title: "Chemistry and Materials Science",
+        link: "chemistry",
+      },
+      { title: "Civil Engineering", link: "civil" },
+      { title: "Computer Science", link: "computer" },
+      { title: "Design", link: "design" },
+      { title: "Economics", link: "economics" },
+      {
+        title: "Electronics and Nanoengineering",
+        link: "electronics",
+      },
+      {
+        title: "Electrical Engineering and Automation",
+        link: "electrical",
+      },
+      {
+        title: "Film Television and Scenography",
+        link: "film",
+      },
+      { title: "Finance", link: "finance" },
+      {
+        title: "Industrial Engineering and Management",
+        link: "industrial",
+      },
+      {
+        title: "Information and Communications Engineering",
+        link: "information",
+      },
+      {
+        title: "Information and Service Management",
+        link: "information",
+      },
+      {
+        title: "Management Studies",
+        link: "management",
+      },
+      { title: "Marketing", link: "marketing" },
+      {
+        title: "Mathematics and Systems Analysis",
+        link: "mathematics",
+      },
+      {
+        title: "Mechanical Engineering",
+        link: "mechanical",
+      },
+      {
+        title: "Neuroscience and Biomedical Engineering",
+        link: "neuroscience",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    data: [
+      {
+        title: "Facilities and space reservations",
+        link: "facilities",
+      },
+      { title: "Aalto Space", link: "aalto" },
+      { title: "Asio", link: "asio" },
+      { title: "AV and Web malfunctions", link: "av" },
+      {
+        title: "Maintenance requests",
+        link: "maintenance",
+      },
+      {
+        title: "Cleaning removal and furniture service requests",
+        link: "cleaning",
+      },
+      {
+        title: "Safety and emergencies",
+        link: "safety",
+      },
+      { title: "Emergencies", link: "emergencies" },
+      { title: "Incident report", link: "incident" },
+    ],
+  },
+  {
+    title: "About us",
+    data: [
+      { title: "About us", link: "about" },
+      { title: "Our history", link: "our" },
+      {
+        title: "Schools departments and units",
+        link: "schools",
+      },
+      { title: "Campus", link: "campus" },
+      { title: "Careers", link: "careers" },
+      { title: "Our strategy", link: "our" },
+      { title: "Code of conduct", link: "code" },
+      {
+        title: "People and contact information",
+        link: "people",
+      },
+      { title: "Archives", link: "archives" },
+      { title: "Rankings", link: "rankings" },
+      {
+        title: "Library  Learning Centre",
+        link: "library",
+      },
+      {
+        title: "Sustainability",
+        link: "sustainability",
+      },
+      { title: "Management", link: "management" },
+    ],
+  },
+  {
+    title: "Collaboration",
+    data: [
+      {
+        title: "Collaborate with us",
+        link: "collaborate",
+      },
+      { title: "For alumni", link: "for" },
+      {
+        title: "Corporate collaboration",
+        link: "corporate",
+      },
+      {
+        title: "Entrepreneurship and innovations",
+        link: "entrepreneurship",
+      },
+      {
+        title: "International collaboration",
+        link: "international",
+      },
+      {
+        title: "Collaboration with cities and regions",
+        link: "collaboration",
+      },
+      { title: "Support us", link: "support" },
+    ],
+  },
+];
