30e849c0eb3275322941fd634731da61a6258e14
estherniyo <estherniyonkuru123@gmail.com>
2023-02-28 16:05:12 +0200

    the first code refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 import { BiChevronDown, BiLockAlt, BiMenu, BiSearch } from "react-icons/bi";
 import { CiHeart } from "react-icons/ci";
 import { IoGlobeSharp } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
+import { FaResearchgate } from "react-icons/fa";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(0);
   const [sticky, setSticky] = useState(scrolled);
+  const [slide, setSlide] = useState(1);
+  const [tabValue, setTabValue] = useState(0);
 
   useEffect(() => {
     const changescrolled = () => {
@@ -30,6 +34,11 @@ const Navbar = () => {
       window.removeEventListener("scroll", changesticky);
     };
   }, [sticky]);
+
+  useEffect(() => {
+    document.body.style.overflow = tabValue ? "hidden" : "auto";
+    return () => (document.body.style.overflow = "scroll");
+  }, [tabValue]);
   return (
     <>
       {" "}
@@ -88,10 +97,15 @@ const Navbar = () => {
               <span>EN</span>
               <BiChevronDown />{" "}
             </li>
-            <li className="flex items-center gap-2 hover:bg-lightGray p-1">
+            <Link
+              href={""}
+              onClick={() => setTabValue(1)}
+              className="flex items-center gap-2 hover:bg-lightGray p-1 "
+            >
               <BiSearch />
+
               <span>Search</span>
-            </li>
+            </Link>
             <li className="flex items-center gap-2 hover:bg-lightGray p-1">
               <BiMenu /> <span>Menu</span>
             </li>
@@ -138,6 +152,40 @@ const Navbar = () => {
           <BiSearch size={20} className="pt-1" />
           <span>|</span>
           <p>Menu</p>
+        </div>
+      </div>
+      {/* search content */}
+      <div
+        className={
+          tabValue
+            ? "w-screen text-white absolute mx-auto h-screen transition-all ease-in-out duration-300 opacity-100 inset-0 z-[10000000] overflow-hidden gradient"
+            : "transition-all duration-0 absolute opacity-0 h-0 bottom-0 overflow-hidden mx-auto"
+        }
+      >
+        <div className="max-w-3xl items-center pt-12 mx-auto flex w-full justify-between">
+          <p className="text-xl font-semibold">Search</p>
+          <div className=" flex justify-end">
+            <div className="bg-gray p-5">
+              <RxCross1
+                className={`self-end hover:border w-10 h-10 p-2 rounded-full ${
+                  tabValue === 1 ? " " : ""
+                }`}
+                onClick={() => {
+                  setTabValue(0), setSlide(1);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="h-6"></div>
+        <div className="max-w-4xl flex justify-between items-center px-6 py-2 mx-auto border-2 rounded-full border-white bg-transparent">
+          <input
+            type=""
+            placeholder=""
+            className="outline-none bg-transparent"
+          />
+          <BiSearch size={25} />
         </div>
       </div>
     </>
